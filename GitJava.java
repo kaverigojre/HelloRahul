5d19131ef9372bca99ae10886a482816fd517d1e
kaverigojre <kaverigojre@gmail.com>
2017-06-06 03:44:49 +0530

    Added BRanch b1

--- GitJava.java
+++ GitJava.java
@@ -3,8 +3,8 @@ class GitJava
    public static void main(String args[])
   {
 
-    System.out.println("HEllo Rahul ");
+    System.out.println("HEllo Rahul...I'm creating new BRanch here");
   }
 
-//second commit  git diff
+//This is Branch
 }
